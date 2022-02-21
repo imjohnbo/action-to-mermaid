49663ce32190675f0a9254843966109ee1a7e0c1
John Bohannon <imjohnbo@github.com>
2022-02-21 13:30:02 -0500

    chore: add linters

--- dist/index.js
+++ dist/index.js
@@ -15997,189 +15997,188 @@ const font = __nccwpck_require__(291);
 
 run();
 
-async function run() {
-    try {
-        const token = core.getInput('token');
-        const nwo = core.getInput('nwo');
-        const octokit = github.getOctokit(token);
+async function run () {
+  try {
+    const token = core.getInput('token');
+    const nwo = core.getInput('nwo');
+    const octokit = github.getOctokit(token);
 
-        if (!isValidAction(nwo)) {
-            throw new Error('Action is not of format "owner/name".');
-        }
-
-        const { contents, aInYaml } = await getAction(nwo, octokit);
-
-        if (!contents) {
-            throw new Error(`${nwo} doesn't appear to have an action.yml or action.yaml.`);
-        }
-
-        const mermaid = convertActionToMermaid(contents, { nwo, aInYaml });
-
-        // '```mermaid\n```' length is 14
-        if (mermaid.length === 14) {
-            throw new Error('Action definition is empty');
-        }
-
-        core.setOutput('mermaid', mermaid);
-    } catch (error) {
-        core.setFailed(error.message);
+    if (!isValidAction(nwo)) {
+      throw new Error('Action is not of format "owner/name".');
     }
+
+    const { contents, aInYaml } = await getAction(nwo, octokit);
+
+    if (!contents) {
+      throw new Error(`${nwo} doesn't appear to have an action.yml or action.yaml.`);
+    }
+
+    const mermaid = convertActionToMermaid(contents, { nwo, aInYaml });
+
+    // '```mermaid\n```' length is 14
+    if (mermaid.length === 14) {
+      throw new Error('Action definition is empty');
+    }
+
+    core.setOutput('mermaid', mermaid);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 // get action.yml (preferred) or action.yaml at path
-async function getAction(nwo, octokit) {
-    const owner = nwo.split('/')[0];
-    const repo = nwo.split('/')[1];
-    core.debug(`Getting metadata for action: ${nwo}`);
-    
-    try {
-        let { data: contents } = await octokit.rest.repos.getContent({
-            owner,
-            repo,
-            path: 'action.yml',
-            mediaType: {
-                format: 'raw'
-            }
-        });
-        if (contents) {
-            return { 
-                contents,
-                aInYaml: false
-            };
-        }
-    } catch (error) {
-        let { data: contents } = await octokit.rest.repos.getContent({
-            owner,
-            repo,
-            path: 'action.yaml',
-            mediaType: {
-                format: 'raw'
-            }
-        });
-        if (contents) {
-            return {
-                contents,
-                aInYaml: true
-            };
-        }
-        else {
-            return {
-                contents: null
-            };
-        }
+async function getAction (nwo, octokit) {
+  const owner = nwo.split('/')[0];
+  const repo = nwo.split('/')[1];
+  core.debug(`Getting metadata for action: ${nwo}`);
+
+  try {
+    const { data: contents } = await octokit.rest.repos.getContent({
+      owner,
+      repo,
+      path: 'action.yml',
+      mediaType: {
+        format: 'raw'
+      }
+    });
+    if (contents) {
+      return {
+        contents,
+        aInYaml: false
+      };
     }
+  } catch (error) {
+    const { data: contents } = await octokit.rest.repos.getContent({
+      owner,
+      repo,
+      path: 'action.yaml',
+      mediaType: {
+        format: 'raw'
+      }
+    });
+    if (contents) {
+      return {
+        contents,
+        aInYaml: true
+      };
+    } else {
+      return {
+        contents: null
+      };
+    }
+  }
 }
 
 // Check if action is of format "owner/name"
-function isValidAction(nwo) {
-    const regex = /^[a-zA-Z0-9-_]+\/[a-zA-Z0-9-_]+$/;
-    return regex.test(nwo);
+function isValidAction (nwo) {
+  const regex = /^[a-zA-Z0-9-_]+\/[a-zA-Z0-9-_]+$/;
+  return regex.test(nwo);
 }
 
-function convertActionToMermaid(contents, options) {
-    core.debug(`Converting content to mermaid: ${contents}`);
-    const json = yaml.parse(contents);
-    core.debug(`Converted yaml as json: ${JSON.stringify(json)}`);
-    if (json && Object.keys(json).length === 0) {
-        throw new Error('Action is empty');
+function convertActionToMermaid (contents, options) {
+  core.debug(`Converting content to mermaid: ${contents}`);
+  const json = yaml.parse(contents);
+  core.debug(`Converted yaml as json: ${JSON.stringify(json)}`);
+  if (json && Object.keys(json).length === 0) {
+    throw new Error('Action is empty');
+  }
+  let mermaid = '```mermaid\n';
+  mermaid += 'flowchart LR\n';
+  const name = getName(json);
+  mermaid += handleInputs(json, name);
+  mermaid += handleOutputs(json, name);
+  mermaid += handleClassDefs(json);
+  mermaid += handleClicks(json, { contents, nwo: options.nwo, aInYaml: options.aInYaml });
+  mermaid += '```';
+  core.debug(`Converted mermaid: ${mermaid}`);
+  return mermaid;
+}
+
+function getName (json) {
+  const name = json.name;
+  if (!name) {
+    throw new Error('Action does not have a name');
+  }
+  return name;
+}
+
+function handleInputs (json, name) {
+  core.debug(`Handling inputs for action: ${name}`);
+  const inputs = json.inputs ? Object.keys(json.inputs) : [];
+  let str = '';
+  for (const input of inputs) {
+    const required = json.inputs[input].required ? 'required' : 'optional';
+    str += `${input}:::${required}-->action(${name}):::action\n`;
+  }
+  return str;
+}
+
+function handleOutputs (json, name) {
+  core.debug(`Handling output for action: ${name}`);
+  const outputs = json.outputs ? Object.keys(json.outputs) : [];
+  let str = '';
+  for (const output of outputs) {
+    str += `action(${name})-->${output}:::output\n`;
+  }
+  return str;
+}
+
+function handleClassDefs (json) {
+  core.debug(`Handling class defs for action: ${json.name}`);
+  let classDef = '';
+  classDef += 'classDef required fill:#6ba06a,stroke:#333,stroke-width:3px\n';
+  classDef += 'classDef optional fill:#d9b430,stroke:#333,stroke-width:3px\n';
+
+  // decide whether white or black text based on fill color
+  const background = json.branding ? json.branding.color : '#a2a5a9';
+  const fontColor = font.default(background);
+
+  classDef += `classDef action fill:${background},stroke:#333,stroke-width:3px,color:${fontColor}\n`;
+  classDef += 'classDef output fill:#fff,stroke:#333,stroke-width:3px,color:#333\n';
+  return classDef;
+}
+
+function handleClicks (json, options) {
+  core.debug(`Handling clicks for action: ${json.name}`);
+  let str = '';
+
+  // get a list of all the inputs and their matching line numbers
+  const inputs = json.inputs ? Object.keys(json.inputs) : [];
+  const inputLines = {};
+  for (const input of inputs) {
+    const line = getLineNumber(options.contents, input);
+    inputLines[input] = line;
+    str += `click ${input} "${makeClickLink(options.nwo, options.aInYaml, line)}"\n`;
+  }
+  // get a list of all the outputs and their matching line numbers
+  const outputs = json.outputs ? Object.keys(json.outputs) : [];
+  const outputLines = {};
+  for (const output of outputs) {
+    const line = getLineNumber(options.contents, output);
+    outputLines[output] = line;
+    str += `click ${output} "${makeClickLink(options.nwo, options.aInYaml, line)}"\n`;
+  }
+  core.debug(`inputLines: ${JSON.stringify(inputLines)}`);
+  core.debug(`outputLines: ${JSON.stringify(outputLines)}`);
+
+  return str;
+}
+
+function makeClickLink (nwo, aInYaml, line) {
+  return `${process.env.GITHUB_SERVER_URL}/${nwo}/blob/main/action.y${aInYaml ? 'a' : ''}ml#L${line}`;
+}
+
+function getLineNumber (contents, input) {
+  const regex = new RegExp(`${input}`, 'g');
+  const lines = contents.split('\n');
+  let line = 1;
+  for (const l of lines) {
+    if (regex.test(l)) {
+      return line;
     }
-    let mermaid = '```mermaid\n';
-    mermaid += `flowchart LR\n`;
-    const name = getName(json);
-    mermaid += handleInputs(json, name);
-    mermaid += handleOutputs(json, name);
-    mermaid += handleClassDefs(json);
-    mermaid += handleClicks(json, { contents, nwo: options.nwo, aInYaml: options.aInYaml });
-    mermaid += '```';
-    core.debug(`Converted mermaid: ${mermaid}`);
-    return mermaid;
-}
-
-function getName(json) {
-    const name = json.name;
-    if (!name) {
-        throw new Error('Action does not have a name');
-    }
-    return name;
-}
-
-function handleInputs(json, name) {
-    core.debug(`Handling inputs for action: ${name}`);
-    const inputs = json.inputs ? Object.keys(json.inputs) : [];
-    let str = ``;
-    for (const input of inputs) {
-        const required = json.inputs[input].required ? 'required' : 'optional';
-        str += `${input}:::${required}-->action(${name}):::action\n`;
-    }
-    return str;
-}
-
-function handleOutputs(json, name) {
-    core.debug(`Handling output for action: ${name}`);
-    const outputs = json.outputs ? Object.keys(json.outputs) : [];
-    let str = ``;
-    for (const output of outputs) {
-        str += `action(${name})-->${output}:::output\n`;
-    }
-    return str;
-}
-
-function handleClassDefs(json) {
-    core.debug(`Handling class defs for action: ${json.name}`);
-    let classDef = ``;
-    classDef += `classDef required fill:#6ba06a,stroke:#333,stroke-width:3px\n`;
-    classDef += `classDef optional fill:#d9b430,stroke:#333,stroke-width:3px\n`;
-    
-    // decide whether white or black text based on fill color
-    const background = json['branding'] ? json['branding']['color'] : '#a2a5a9';
-    const fontColor = font.default(background);
-
-    classDef += `classDef action fill:${background},stroke:#333,stroke-width:3px,color:${fontColor}\n`;
-    classDef += `classDef output fill:#fff,stroke:#333,stroke-width:3px,color:#333\n`;
-    return classDef;
-}
-
-function handleClicks(json, options) {
-    core.debug(`Handling clicks for action: ${json.name}`);
-    let str = ``;
-
-    // get a list of all the inputs and their matching line numbers
-    const inputs = json.inputs ? Object.keys(json.inputs) : [];
-    const inputLines = {};
-    for (const input of inputs) {
-        const line = getLineNumber(options.contents, input);
-        inputLines[input] = line;
-        str += `click ${input} "${makeClickLink(options.nwo, options.aInYaml, line)}"\n`;
-    }
-    // get a list of all the outputs and their matching line numbers
-    const outputs = json.outputs ? Object.keys(json.outputs) : [];
-    const outputLines = {};
-    for (const output of outputs) {
-        const line = getLineNumber(options.contents, output);
-        outputLines[output] = line;
-        str += `click ${output} "${makeClickLink(options.nwo, options.aInYaml, line)}"\n`;
-    }
-    core.debug(`inputLines: ${JSON.stringify(inputLines)}`);
-    core.debug(`outputLines: ${JSON.stringify(outputLines)}`);
-
-    return str;
-}
-
-function makeClickLink(nwo, aInYaml, line) {
-    return `${process.env.GITHUB_SERVER_URL}/${nwo}/blob/main/action.y${aInYaml ? 'a' : ''}ml#L${line}`;
-}
-
-function getLineNumber(contents, input) {
-    const regex = new RegExp(`${input}`, 'g');
-    const lines = contents.split('\n');
-    let line = 1;
-    for (const l of lines) {
-        if (regex.test(l)) {
-            return line;
-        }
-        line++;
-    }
-    return null;
+    line++;
+  }
+  return null;
 }
 
 })();
